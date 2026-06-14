d53348fe81bf87996062204cae5cc992dc9aaf92
anisbouhadida <anisbouhadida.dev@gmail.com>
2026-06-14 16:23:10 +0200

    test: skip context smoke test without Docker

--- src/test/java/dz/anisbouhadida/medzloader/MedzLoaderApplicationTests.java
+++ src/test/java/dz/anisbouhadida/medzloader/MedzLoaderApplicationTests.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Import(PostgresTestContainerConfiguration.class)
+@Testcontainers(disabledWithoutDocker = true)
 class MedzLoaderApplicationTests {
 
   @Test
